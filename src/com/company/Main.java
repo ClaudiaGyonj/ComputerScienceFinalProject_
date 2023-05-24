f1f5c0074e5ca9ce47831f004fe557098d4e156e
Brinda Moudgalya <brinda.moudgalya@gmail.com>
2023-05-23 18:55:41 -0700

    actual commit #1 - base of dotProduct app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,30 @@
 package com.company;
+import java.util.Scanner;
+import java.util.Scanner.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // can you see this change?
-        System.out.println("Hello World 000");
-        System.out.println("Hello World 123");
+
+    }
+    public int [][] dotProduct () {
+        Scanner keyboard = new Scanner(System.in);
+
+        // asking user how many items/users/features they want
+        System.out.println("How many items are there?");
+        int numItems = keyboard.nextInt();
+        System.out.println("How many users are there?");
+        int numUsers = keyboard.nextInt();
+        System.out.println("How many features are there?");
+        int numFeatures = keyboard.nextInt();
+
+        // creating 2D arrays with the number of items/users/features requested
+        int [][] userItem = new int[numUsers][numItems];
+        int [][] itemFeature = new int[numItems][numFeatures];
+        int [][] userFeature = new int[numUsers][numFeatures];
+
+        //... more code
+
+        return userItem;
     }
 }
