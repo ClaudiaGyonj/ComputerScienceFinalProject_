2292812073a769cffd1b226426f230d9044c132f
Brinda Moudgalya <brinda.moudgalya@gmail.com>
2023-05-25 13:02:50 -0700

    commit 2 - made seperate methods for getting numbers of users/items/features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,48 @@
 package com.company;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Scanner.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int numItems;
+    public static int numUsers;
+    public static int numFeatures;
+    public static int [][] userItem;
+    public static int [][] itemFeature;
+    public static int [][] userFeature;
 
-    }
-    public int [][] dotProduct () {
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
         // asking user how many items/users/features they want
-        System.out.println("How many items are there?");
-        int numItems = keyboard.nextInt();
-        System.out.println("How many users are there?");
-        int numUsers = keyboard.nextInt();
-        System.out.println("How many features are there?");
-        int numFeatures = keyboard.nextInt();
+        numItems = getNumItems(keyboard);
+        numUsers = getNumUsers(keyboard);
+        numFeatures = getNumFeatures(keyboard);
 
         // creating 2D arrays with the number of items/users/features requested
-        int [][] userItem = new int[numUsers][numItems];
-        int [][] itemFeature = new int[numItems][numFeatures];
-        int [][] userFeature = new int[numUsers][numFeatures];
+        userItem = new int[numUsers][numItems];
+        itemFeature = new int[numFeatures][numItems];
+        userFeature = new int[numUsers][numFeatures];
 
-        //... more code
-
-        return userItem;
+        System.out.println(Arrays.deepToString(userItem));
+        System.out.println(Arrays.deepToString(itemFeature));
+        System.out.println(Arrays.deepToString(userFeature));
     }
+    public static int getNumItems(Scanner keyboard){
+        System.out.println("How many items are there?");
+        int numItems = keyboard.nextInt();
+        return numItems;
+    }
+    public static int getNumUsers(Scanner keyboard) {
+        System.out.println("How many users are there?");
+        int numUsers = keyboard.nextInt();
+        return numUsers;
+    }
+    public static int getNumFeatures(Scanner keyboard) {
+        System.out.println("How many features are there?");
+        int numFeatures = keyboard.nextInt();
+        return numFeatures;
+    }
+
 }
